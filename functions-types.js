a31c5230c4b2804102b0361a68b454fdd5d8c86d
ricardo.fahham <ricardo.fahham@WP100057NRJJB.local>
2023-05-25 09:42:45 -0300

    tipos de funções

---
+++ functions-types.js
@@ -0,0 +1,14 @@
+//function declaration
+// function soma(a, b) {
+//     return a + b;
+// }
+
+//function expression
+// const soma = function(a, b) {
+//     return a + b;
+// }
+
+//arrow function
+const soma = (a, b) => a + b;
+
+console.log(soma(15, 15))
