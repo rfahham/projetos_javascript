b939aba7ecb275add98de860fea7cdd4d0cc37fb
Ricardo Fahham <rfahham@hotmail.com>
2023-07-17 11:51:12 -0300

    find item in object

---
+++ jsonstring/find.js
@@ -0,0 +1,6 @@
+let obj = { name: "Ricardo Fahham", age: 52, adress: "Rua General", city: "Petrópolis"};
+
+console.log(obj.hasOwnProperty('name'));
+console.log(obj.hasOwnProperty('age'));
+console.log(obj.hasOwnProperty('city'));
+console.log(obj.hasOwnProperty('adress'));
