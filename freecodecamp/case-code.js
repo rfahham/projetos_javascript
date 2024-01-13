eba9ce4000ce753e578eb78337ff2a1bb86c36bb
Ricardo Fahham <rfahham@hotmail.com>
2024-01-13 10:04:41 -0300

    case-code

---
+++ freecodecamp/case-code.js
@@ -0,0 +1,96 @@
+function caseInSwitch(val) {
+    
+    let answer = "";
+
+    switch (val) {
+    case a:
+      answer = "ALPHA";
+      break;
+    case b:
+      answer = "BRAVO";
+      break;
+    case c:
+      answer = "CHARLIE";
+      break;
+    case d:
+      answer = "DELTA";
+      break;
+    case e:
+      answer = "ECHO";
+      break;
+    case f:
+      answer = "FOXTROT";
+      break;
+    case g:
+      answer = "GOLF";
+      break;
+    case h:
+      answer = "HOTEL";
+      break;
+    case i:
+      answer = "INDIA";
+      break;
+    case j:
+      answer = "JULIET";
+      break;
+    case k:
+      answer = "KILO";
+      break;
+    case l:
+      answer = "LIMA";
+      break;
+    case m:
+      answer = "MIKE";
+      break;
+    case n:
+      answer = "NOVEMBER";
+      break;
+    case o:
+      answer = "OSCAR";
+      break;
+    case p:
+      answer = "PAPA";
+      break;
+    case q:
+      answer = "QUEBEC";
+      break;
+    case r:
+      answer = "ROMEO";
+      break;
+    case s:
+      answer = "SIERRA";
+      break;
+    case t:
+      answer = "TANGO";
+      break;
+    case u:
+      answer = "UNIFORM";
+      break;
+    case v:
+      answer = "VITOR";
+      break;
+    case w:
+      answer = "WHISKEY";
+      break;
+    case x:
+      answer = "X-RAY";
+      break;
+    case y:
+      answer = "YANKEE";
+      break;
+    case z:
+      answer = "ZULU";
+      break; 
+  }
+    return answer;
+  }
+  
+  const result1 = caseInSwitch(1);
+  const result2 = caseInSwitch(2);
+  const result3 = caseInSwitch(3);
+  const result4 = caseInSwitch(4);
+
+  console.log(result1)
+  console.log(result2)
+  console.log(result3)
+  console.log(result4)
