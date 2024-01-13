4e6dcf0bfe380e35b902a4f70816dbd9bdf55cee
Ricardo Fahham <rfahham@hotmail.com>
2024-01-13 16:47:08 -0300

    sobre objeto em javascript

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 [Work with Functions] (functions)
 
 [Work with Arrays] (work-array)
+
---
+++ objeto/main.js
@@ -0,0 +1,20 @@
+const pessoas = [ 
+    { nome : "Ricardo", idade : 53 },
+    { nome : "Alessandra", idade : 50 },
+    { nome : "João Ricardo", idade : 24 },
+    { nome : "João Victor", idade : 22 },
+    { nome : "Andreza", idade : 18 }
+];
+
+
+
+const obj1 = {
+    nome: 'Ricardo'
+}
+
+const obj2 = {
+    nome: 'Alessandra'
+}
+
+console.log(obj1)
+console.log(obj2)
