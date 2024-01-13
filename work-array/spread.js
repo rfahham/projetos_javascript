dfdfcf729344f8def9ff66d276ed319dbf8272d9
Ricardo Fahham <rfahham@hotmail.com>
2024-01-13 17:01:16 -0300

    Spread Operator - clona o array original

---
+++ work-array/spread.js
@@ -0,0 +1,8 @@
+// spread operator
+
+// Clona o array original
+
+const arrayOriginal = [1,2,3];
+const arrayClonada = [...arrayOriginal];
+
+console.log('Array Clonada do array origina', arrayClonada)
