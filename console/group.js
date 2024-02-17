6ce0a80d3ea01be83d5e77661f65036757071aa4
Ricardo Fahham <rfahham@hotmail.com>
2024-02-17 10:49:45 -0300

    add console-groups

---
+++ console/group.js
@@ -0,0 +1,13 @@
+console.group("Fruits");
+console.log("Apple");
+console.log("Banana");
+console.log("Orange");
+console.groupEnd();
+
+console.group("Vegetables");
+console.log("Carrot");
+console.log("Broccoli");
+console.log("Tomato");
+console.groupEnd();
+
+// node group.js
