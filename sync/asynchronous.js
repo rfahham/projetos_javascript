59560242afed87a9d29db6f960736d4e815dfa64
Ricardo Fahham <rfahham@gmail.com>
2024-05-15 12:17:53 -0300

    Synchronous-Asynchronous

---
+++ sync/asynchronous.js
@@ -0,0 +1,8 @@
+console.log("Boiling water...")
+
+// Wait for 3 seconds
+setTimeout(function() {
+    console.log("Water Boiled!");
+}, 300)
+
+console.log("Preparing tea bag...")
---
+++ sync/synchronous.js
@@ -0,0 +1,5 @@
+console.log("Mixing ingredients...")
+
+console.log("Putting the cake in the oven...")
+
+console.log("Cake is ready!")
